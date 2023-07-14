47d2ac8b9ab03a9132a36b2c39f00ef6b88e57fa
RubenEmeis <102186025+RubenEmeis@users.noreply.github.com>
2023-07-14 10:26:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 curl https://raw.githubusercontent.com/jupyterhub/the-littlest-jupyterhub/master/bootstrap/bootstrap.py \
 | sudo python3 - --admin ${1}
-    --plugin git+https://github.com/kafonek/tljh-shared-directory
     --user-requirements-txt-URL https://raw.githubusercontent.com/RubenEmeis/RubenEmeis/main/requirements.txt
+    --plugin git+https://github.com/kafonek/tljh-shared-directory
+    
 
