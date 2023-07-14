743190f0cb3d3c32055a8502661f16924412feb4
RubenEmeis <102186025+RubenEmeis@users.noreply.github.com>
2023-07-14 11:39:38 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,5 @@
+# !/bin/bash	
+
+curl https://raw.githubusercontent.com/jupyterhub/the-littlest-jupyterhub/master/bootstrap/bootstrap.py \
+| sudo python3 - --admin ${1} --user-requirements-txt-URL https://raw.githubusercontent.com/RubenEmeis/RubenEmeis/main/requirements.txt
+
