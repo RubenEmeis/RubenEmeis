87afe01f94b90aec909fb05d07600c0b0d8b1f16
RubenEmeis <102186025+RubenEmeis@users.noreply.github.com>
2023-07-14 12:15:27 +0200

    Delete install_voila.sh

--- install_voila.sh
+++
@@ -1,6 +0,0 @@
-# !/bin/bash	
-
-#!/bin/bash
-curl https://raw.githubusercontent.com/jupyterhub/the-littlest-jupyterhub/master/bootstrap/bootstrap.py \
- | sudo python3 - --admin ${1} \
-   --plugin git+https://github.com/voila-dashboards/tljh-voila-gallery@master#"egg=tljh-voila-gallery&subdirectory=tljh-voila-gallery"
