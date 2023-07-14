50a51fa939fa20062a3baee70c6c6ae194e0cb4d
RubenEmeis <102186025+RubenEmeis@users.noreply.github.com>
2023-07-14 10:25:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 curl https://raw.githubusercontent.com/jupyterhub/the-littlest-jupyterhub/master/bootstrap/bootstrap.py \
 | sudo python3 - --admin ${1}
-    --admin azure_digi_hub --plugin git+https://github.com/kafonek/tljh-shared-directory
+    --plugin git+https://github.com/kafonek/tljh-shared-directory
     --user-requirements-txt-URL https://raw.githubusercontent.com/RubenEmeis/RubenEmeis/main/requirements.txt
 
