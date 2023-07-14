ee78073135ba229001125c314feed3360966b5c0
RubenEmeis <102186025+RubenEmeis@users.noreply.github.com>
2023-07-14 12:27:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 # !/bin/bash	
 
 curl https://raw.githubusercontent.com/jupyterhub/the-littlest-jupyterhub/master/bootstrap/bootstrap.py \
-| sudo python3 - --admin ${1} --user-requirements-txt-URL https://raw.githubusercontent.com/RubenEmeis/RubenEmeis/main/requirements.txt
+| sudo python3 - --admin ${1} --user-requirements-txt-URL https://raw.githubusercontent.com/data-8/materials-sp18/master/requirements.txt
+
 
