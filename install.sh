7cbc206d447d82876d5957a6d8ffd4235220abce
RubenEmeis <102186025+RubenEmeis@users.noreply.github.com>
2023-07-14 11:01:45 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash	
-
-curl https://raw.githubusercontent.com/jupyterhub/the-littlest-jupyterhub/master/bootstrap/bootstrap.py \
-| sudo python3 - \
-    --admin azure_digi_hub --user-requirements-txt-URL https://raw.githubusercontent.com/RubenEmeis/RubenEmeis/main/requirements.txt --plugin git+https://github.com/kafonek/tljh-shared-directory
-    
-
